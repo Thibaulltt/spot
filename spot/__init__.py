71ec1216aeb526adcfe664c5180caf29aa810c9e
Thibault de Villèle <devillelethibault@gmail.com>
2022-11-04 00:01:11 +0100

    py-packaging: hallelujah
    It finally works.
    
    The pyproject.toml file provides basic information about the module
    itself, mainly metadata.
    The setup.py file does the "heavy lifting" by providing build extensions
    invoking CMake at the right time and place.
    All results in a pip-installable module that can then be imported into
    any supported Python distribution (version >= 3.7 for now).

--- spot/__init__.py
+++ spot/__init__.py
@@ -1,3 +1,14 @@
-# Stub to automatically include the generated spot module from pybind11
+"""
+A set of wrappers around the SPOT method : from Sliced Partial Optimal Transport by Bonneel and Coeurjolly (2019).
 
+For more information about the signature and innerworkings of the functions and classes defined within, a look at the
+source code is more than helpful : https://github.com/thibaulltt/spot
+
+All useful classes of this module are defined in a submodule named "_spot", imported here. It has been compiled from C++.
+"""
+
+# Import compiled module from C++ :
 from ._spot import *
+
+# Force set the version information :
+__version__ = _spot.__version__
